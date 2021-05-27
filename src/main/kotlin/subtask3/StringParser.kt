65a8c3b5384df48faaa4f97c4e4216fd64858a82
Tanya <53278501+TatsianaPiatrova@users.noreply.github.com>
2021-05-27 20:59:24 +0300

    Update StringParser.kt

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -6,56 +6,28 @@ class StringParser {
     fun getResult(inputString: String): Array<String> {
         val open = listOf('[','<','(')
         val close = listOf(']','>',')')
-        var result: Array<String> = emptyArray()
-        var first: Int = 0
-        var bracketsopen: Array<Int> = emptyArray()
-        var bracketsclose: Array<Int> = emptyArray()
-        while(first <= inputString.length-1) {
-            var word = inputString[first]
-            var i =0
-            if (word in open)
-                bracketsopen += first
-            if (word in close)
-                bracketsclose += first
-            first++
-        }
-        for (br in bracketsopen)
-            println(br)
+        var result = ArrayList<String>()
+        val input = StringBuilder(inputString)
         var clone:Int = 0
-        first = 0
-        var flag = false
-        while (first <= inputString.length-1)
+        for (i in input.length-1 downTo 0 )
         {
-            clone = first
-            var word = inputString[first]
-            if (first in bracketsopen)
+            if (input[i] in open)
             {
-                while(clone <= inputString.length-1)
+                var clone = 0
+                for (j in i until input.length)
                 {
-                    if (clone in bracketsclose && open.indexOf(word)==close.indexOf(inputString[clone])) {
-                        if (first == 6) {
-                            println(inputString.substring(first+1, 181))
-                            result += inputString.substring(first+1, 181)
-                            break
-                        }
-
-                        println(inputString.substring(first+1, clone))
-                        result += inputString.substring(first+1, clone)
+                    if (input[j] in close && open.indexOf(input[i])==close.indexOf(inputString[j])) {
+                        clone=j
+                        input[i]=' '
+                        input[clone]=' '
+                        println(inputString.substring(i+1, clone))
                         break
                     }
-
-                    clone++
                 }
+                result.add(0, inputString.substring(i + 1, clone))
             }
-            first++
-        }
-        if (result.size>200) {
-            var buf: String
-            buf = result[2]
-            result[2] = result[3]
-            result[3] = buf
         }
         println(result.joinToString(";"))
-        return result
+        return result.toTypedArray()
     }
 }
