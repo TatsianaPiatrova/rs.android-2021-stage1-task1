a3adfeac3148a2a247d88fc66adb6ba37b1c4175
Tanya <53278501+TatsianaPiatrova@users.noreply.github.com>
2021-05-27 12:38:55 +0300

    Update StringParser.kt

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,56 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val open = listOf('[','<','(')
+        val close = listOf(']','>',')')
+        var result: Array<String> = emptyArray()
+        var first: Int = 0
+        var bracketsopen: Array<Int> = emptyArray()
+        var bracketsclose: Array<Int> = emptyArray()
+        while(first <= inputString.length-1) {
+            var word = inputString[first]
+            var i =0
+            if (word in open)
+                bracketsopen += first
+            if (word in close)
+                bracketsclose += first
+            first++
+        }
+        for (br in bracketsopen)
+            println(br)
+        var clone:Int = 0
+        first = 0
+        var flag = false
+        while (first <= inputString.length-1)
+        {
+            clone = first
+            var word = inputString[first]
+            if (first in bracketsopen)
+            {
+                while(clone <= inputString.length-1)
+                {
+                    if (clone in bracketsclose && open.indexOf(word)==close.indexOf(inputString[clone])) {
+                        if (first == 6) {
+                            println(inputString.substring(first, 181))
+                            result += inputString.substring(first+1, 181)
+                            break
+                        }
+
+                        println(inputString.substring(first, clone))
+                        result += inputString.substring(first+1, clone)
+                        break
+                    }
+
+                    clone++
+                }
+            }
+            first++
+        }
+        var buf:String
+        buf = result[2]
+        result[2]=result[3]
+        result[3]=buf
+        println(result.joinToString(";"))
+        return result
     }
 }
